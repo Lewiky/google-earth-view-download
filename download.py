bdd41de52e85d804b1c486a47bfe54e2ef6abf95
Lewis Bell <lewisbell999@gmail.com>
2018-08-14 15:53:36 +0100

    apease charlie again

--- download.py
+++ download.py
@@ -3,15 +3,15 @@ import requests
 import shutil
 from sys import argv
 
-def download():
+def download(num, path):
     passed = 0
     skipped = 0
-    for i in range(1000, int(argv[1])+1000):
+    for i in range(1000, int(num+1000):
         path = f'https://www.gstatic.com/prettyearth/assets/full/{i}.jpg'
         r = requests.head(path)
         if r.headers['content-type'] == 'image/jpeg':
             p = requests.get(path, stream=True)
-            with open(f'{argv[2]}/{i}.jpg', 'wb') as f:
+            with open(f'{path}/{i}.jpg', 'wb') as f:
                 shutil.copyfileobj(p.raw, f)
             passed += 1
         else:
@@ -20,4 +20,4 @@ def download():
     print("")
 
 if __name__ == '__main__':
-    download()
+    download(argv[1], argv[2])
